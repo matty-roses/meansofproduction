d09ec5fa16cff2fe7da9048bb953babe9380ec7d
Matty Roses <debsnroses@protonmail.com>
2021-12-28 17:25:42 -0500

    Logos and basic page stuff

--- mop-webclient/src/App.tsx
+++ mop-webclient/src/App.tsx
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Means of Production
+        </h1>
+        <p>Libraries of Everything</p>
       </header>
     </div>
   );
