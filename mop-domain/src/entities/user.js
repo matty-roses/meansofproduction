ba76c34db3f5b97ef1204984df2db7a23d6659d4
Matty Roses <debsnroses@protonmail.com>
2021-12-28 15:32:57 -0500

    Remove artifacts

--- mop-domain/src/entities/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
-const person_1 = require("./person");
-class User extends person_1.Person {
-}
-exports.User = User;
-//# sourceMappingURL=user.js.map
